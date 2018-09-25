1f856c1b79eb7a5e1282599abcc2d88a37fd9fc2
duypham <duypham@192.168.1.116>
2018-09-25 22:46:15 +0700

    CityModel added

--- TechTrade/src/main/java/com/green/finale/model/CityModel.java
+++ TechTrade/src/main/java/com/green/finale/model/CityModel.java
@@ -1,18 +1,44 @@
+/**
+ * 
+ */
 package com.green.finale.model;
-
-import java.util.List;
 
 import com.green.finale.entity.City;
 
+/**
+ * @author duypham
+ *
+ */
 public class CityModel {
-	private List<City> cityList;
+	private String id;
+	private String name;
 
-	public List<City> getCityList() {
-		return cityList;
+	public String getId() {
+		return id;
 	}
 
-	public void setCityList(List<City> cityList) {
-		this.cityList = cityList;
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void fromCity(City city) {
+		this.setId(city.getId());
+		this.setName(city.getName());
+	}
+	
+	public City toCity() {
+		City city = new City();
+		city.setId(this.getId());
+		city.setName(this.getName());
+		return city;
 	}
 
 }
